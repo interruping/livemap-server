b74246d857349901bd6839eb8ff79f4a186aa114
geonyounglim <interruping@naver.com>
2017-09-26 23:36:52 +0900

    add  service_type.hpp

---
+++ src/dylibsrc/service_type.hpp
@@ -0,0 +1,23 @@
+//
+//  service_type.hpp
+//  livemap_server
+//
+//  Created by Geon young Lim on 2017. 9. 26..
+//
+
+#ifndef service_type_h
+#define service_type_h
+
+enum service_type {
+    DEFAULT = 0,
+    USERNODEUPDATE = 1,
+    REQUESTUSERINFO = 2,
+    SETUSERINFO = 3,
+    NEARNODEINFO = 4,
+    USERVIEWPOINTUPDATE = 5,
+    UTF8MESSAGESEND = 6
+    
+};
+
+
+#endif /* service_type_h */
