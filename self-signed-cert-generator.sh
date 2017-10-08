de009dec639c695fc9601c96ba4e4aa2e329dd78
geonyounglim <interruping@naver.com>
2017-10-08 23:30:16 +0900

    self-signed-cert-generator.sh added and remove default cert and key files.

---
+++ self-signed-cert-generator.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Generate private key..."
+openssl genrsa -des3 -out server.key 1024 > /dev/null
+echo "Generate certificate signing request..."
+openssl req -new -key server.key -out server.csr
+echo "Sign certificate with private key..."
+openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
+echo "Generate dhparam file..."
+openssl dhparam -out dh2048.pem 2048 > /dev/null
+echo "Move all file."
+mv server.key ./build/debug/
+cp server.crt server.crt.cpy
+mv server.crt ./build/debug/
+mv server.crt.cpy ./testclient/build/debug/server.crt
+mv dh2048.pem ./build/debug/
+echo "Done..."
